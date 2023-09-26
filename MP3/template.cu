d2a18d4aa9e136908845fff64f7572924c0bd44b
ngsmith4 <ngs.new@gmail.com>
2023-09-26 14:17:42 -0500

    MP 3 done

--- MP3/template.cu
+++ MP3/template.cu
@@ -13,14 +13,52 @@
     }                                                                \
   } while (0)
 
+#define TILE_WIDTH 32
+
 // Compute C = A * B
-__global__ void matrixMultiplyShared(float *A, float *B, float *C,
-                                     int numARows, int numAColumns,
-                                     int numBRows, int numBColumns,
-                                     int numCRows, int numCColumns)
+__global__ void matrixMultiply(float *A, float *B, float *C,
+                               int numARows, int numAColumns,
+                               int numBRows, int numBColumns,
+                               int numCRows, int numCColumns)
 {
-  //@@ Insert code to implement matrix multiplication here
-  //@@ You have to use shared memory for this MP
+  __shared__ float subTileA[TILE_WIDTH][TILE_WIDTH];
+  __shared__ float subTileB[TILE_WIDTH][TILE_WIDTH];
+
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int Row = by * TILE_WIDTH + ty;
+  int Col = bx * TILE_WIDTH + tx;
+  int mnum = (numAColumns - 1) / TILE_WIDTH + 1;
+  float sum = 0;
+
+  for (int m = 0; m < mnum; ++m)
+  {
+    if ((Row < numARows) && (m * TILE_WIDTH + tx < numAColumns))
+    {
+      subTileA[ty][tx] = A[Row * numAColumns + m * TILE_WIDTH + tx];
+    }
+    else
+    {
+      subTileA[ty][tx] = 0;
+    }
+    if ((Col < numBColumns) && (m * TILE_WIDTH + ty < numBRows))
+    {
+      subTileB[ty][tx] = B[(m * TILE_WIDTH + ty) * numBColumns + Col];
+    }
+    else
+    {
+      subTileB[ty][tx] = 0;
+    }
+    __syncthreads();
+    for (int k = 0; k < TILE_WIDTH; ++k)
+      sum += subTileA[ty][k] * subTileB[k][tx];
+    __syncthreads();
+  }
+
+  if ((Row < numCRows) && (Col < numCColumns))
+    C[Row * numCColumns + Col] = sum;
 }
 
 int main(int argc, char **argv)
@@ -72,9 +110,9 @@ int main(int argc, char **argv)
 
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
-  //@@ Initialize the grid and block dimensions here REVIEW
-  dim3 DimBlock(BLOCK_WIDTH, BLOCK_WIDTH, 1);
-  dim3 DimGrid(ceil(numBColumns / BLOCK_WIDTH), ceil(numARows / BLOCK_WIDTH), 1);
+  //@@ Initialize the grid and block dimensions here DONE
+  dim3 DimGrid(ceil(((float)numCColumns) / TILE_WIDTH), ceil(((float)numCRows) / TILE_WIDTH), 1);
+  dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1);
 
   wbTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here DONE
