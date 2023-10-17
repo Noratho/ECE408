03f219e3095a409d54e59e570931aa4f3c5c9420
ngsmith4 <ngs.new@gmail.com>
2023-10-16 22:17:56 -0500

    sync push

--- MP4/template.cu
+++ MP4/template.cu
@@ -1,25 +1,81 @@
 #include <wb.h>
 
-#define wbCheck(stmt)                                                     \
-  do {                                                                    \
-    cudaError_t err = stmt;                                               \
-    if (err != cudaSuccess) {                                             \
-      wbLog(ERROR, "CUDA error: ", cudaGetErrorString(err));              \
-      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
-      return -1;                                                          \
-    }                                                                     \
+#define wbCheck(stmt)                                        \
+  do                                                         \
+  {                                                          \
+    cudaError_t err = stmt;                                  \
+    if (err != cudaSuccess)                                  \
+    {                                                        \
+      wbLog(ERROR, "CUDA error: ", cudaGetErrorString(err)); \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);            \
+      return -1;                                             \
+    }                                                        \
   } while (0)
 
 //@@ Define any useful program-wide constants here
+#define TILE_WIDTH 8
+#define MASK_WIDTH 3
 
 //@@ Define constant memory for device kernel here
+__constant__ float M[MASK_WIDTH][MASK_WIDTH][MASK_WIDTH];
 
 __global__ void conv3d(float *input, float *output, const int z_size,
-                       const int y_size, const int x_size) {
+                       const int y_size, const int x_size)
+{
   //@@ Insert kernel code here
+  const int N_width = TILE_WIDTH + MASK_WIDTH - 1;
+  __shared__ float N[N_width][N_width][N_width];
+
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int tz = threadIdx.z;
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int bz = blockIdx.z;
+
+  int col_o = bx * TILE_WIDTH + tx;
+  int row_o = by * TILE_WIDTH + ty;
+  int hei_o = bz * TILE_WIDTH + tz;
+
+  int col_i = col_o - (MASK_WIDTH - 1) / 2;
+  int row_i = row_o - (MASK_WIDTH - 1) / 2;
+  int hei_i = hei_o - (MASK_WIDTH - 1) / 2;
+
+  if (col_i >= 0 && col_i < x_size &&
+      row_i >= 0 && row_i < y_size &&
+      hei_i >= 0 && hei_i < z_size)
+  {
+    int intput_index = hei_i * x_size * y_size + row_i * x_size + col_i;
+    N[tz][ty][tx] = input[intput_index];
+  }
+  else
+  {
+    N[tz][ty][tx] = 0.0f;
+  }
+  __syncthreads();
+
+  float sum = 0.0f;
+  if (tz < TILE_WIDTH && ty < TILE_WIDTH && tx < TILE_WIDTH)
+  {
+    for (int i = 0; i < MASK_WIDTH; i++)
+    {
+      for (int j = 0; j < MASK_WIDTH; j++)
+      {
+        for (int k = 0; k < MASK_WIDTH; k++)
+        {
+          sum += M[i][j][k] * N[i + tz][j + ty][tx + k];
+        }
+      }
+    }
+    if (hei_o < z_size && row_o < y_size && col_o < x_size)
+    {
+      output[hei_o * y_size * x_size + row_o * x_size + col_o] = sum;
+    }
+  }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   wbArg_t args;
   int z_size;
   int y_size;
@@ -50,27 +106,44 @@ int main(int argc, char *argv[]) {
   wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
 
   wbTime_start(GPU, "Doing GPU memory allocation");
-  //@@ Allocate GPU memory here
+  //@@ Allocate GPU memory here DONE
   // Recall that inputLength is 3 elements longer than the input data
   // because the first  three elements were the dimensions
+  int size = (inputLength - 3) * sizeof(float);
+  cudaMalloc((void **)&deviceInput, size);
+  cudaMalloc((void **)&deviceOutput, size);
+
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
-  //@@ Copy input and kernel to GPU here
+  //@@ Copy input and kernel to GPU here DONE
   // Recall that the first three elements of hostInput are dimensions and
   // do
   // not need to be copied to the gpu
+  cudaMemcpy(deviceInput, &hostInput[3], size, cudaMemcpyHostToDevice);
+  cudaMemcpyToSymbol(M, hostKernel, kernelLength * sizeof(float));
+
   wbTime_stop(Copy, "Copying data to the GPU");
 
   wbTime_start(Compute, "Doing the computation on the GPU");
   //@@ Initialize grid and block dimensions here
+  dim3 DimBlock(TILE_WIDTH + MASK_WIDTH - 1,
+                TILE_WIDTH + MASK_WIDTH - 1,
+                TILE_WIDTH + MASK_WIDTH - 1);
+  dim3 DimGrid(ceil(x_size / (1.0 * TILE_WIDTH)),
+               ceil(y_size / (1.0 * TILE_WIDTH)),
+               ceil(z_size / (1.0 * TILE_WIDTH)));
 
   //@@ Launch the GPU kernel here
+  conv3d<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput,
+                                z_size, y_size, x_size);
+
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
   wbTime_start(Copy, "Copying data from the GPU");
   //@@ Copy the device memory back to the host here
+  cudaMemcpy(&hostOutput[3], deviceOutput, size, cudaMemcpyDeviceToHost);
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
   wbTime_stop(Copy, "Copying data from the GPU");
--- Project/custom/cpu-new-forward.cc
+++ Project/custom/cpu-new-forward.cc
@@ -21,22 +21,53 @@ void conv_forward_cpu(float *output, const float *input, const float *mask, cons
     S - stride step length
     */
 
-  const int H_out = (H - K)/S + 1;
-  const int W_out = (W - K)/S + 1;
-  
+  const int H_out = (H - K) / S + 1;
+  const int W_out = (W - K) / S + 1;
 
-  // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-  // An example use of these macros:
-  // float a = in_4d(0,0,0,0)
-  // out_4d(0,0,0,0) = a
-  #define out_4d(i3, i2, i1, i0) output[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-  #define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-  #define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+// We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+// An example use of these macros:
+// float a = in_4d(0,0,0,0)
+// out_4d(0,0,0,0) = a
+#define out_4d(i3, i2, i1, i0) output[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
   // Insert your CPU convolution kernel code here
+  // Loop over each image in the batch
+  for (int b = 0; b < B; ++b)
+  {
+    // Loop over each output feature map
+    for (int m = 0; m < M; ++m)
+    {
+      // Loop over height and width of output feature map
+      for (int h = 0; h < H_out; ++h)
+      {
+        for (int w = 0; w < W_out; ++w)
+        {
+          float sum = 0.0f;
+          // Loop over each input feature map
+          for (int c = 0; c < C; ++c)
+          {
+            // Loop over kernel
+            for (int p = 0; p < K; ++p)
+            {
+              for (int q = 0; q < K; ++q)
+              {
+                int h_in = h * S + p;
+                int w_in = w * S + q;
+                float val = in_4d(b, c, h_in, w_in);
+                float mask_val = mask_4d(m, c, p, q);
+                sum += val * mask_val;
+              }
+            }
+          }
+          out_4d(b, m, h, w) = sum;
+        }
+      }
+    }
+  }
 
-  
-  #undef out_4d
-  #undef in_4d
-  #undef mask_4d
+#undef out_4d
+#undef in_4d
+#undef mask_4d
 }
